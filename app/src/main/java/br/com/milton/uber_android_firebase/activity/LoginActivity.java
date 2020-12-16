6c876f65bcb93b006c8bbe0bc14f4fd89bcf74cb
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-16 15:57:43 -0300

    Interface Cadastro e Login

---
+++ app/src/main/java/br/com/milton/uber_android_firebase/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.com.milton.uber_android_firebase.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.milton.uber_android_firebase.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/br/com/milton/uber_android_firebase/activity/MainActivity.java
+++ app/src/main/java/br/com/milton/uber_android_firebase/activity/MainActivity.java
@@ -2,7 +2,11 @@ package br.com.milton.uber_android_firebase.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Objects;
 
 import br.com.milton.uber_android_firebase.R;
 
@@ -12,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+;
+    }
+
+    public void abrirTelaLogin(View view){
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity( intent );
+    }
+
+    public void abrirTelaCadastro(View view){
+
+        Intent intent = new Intent(this, CadastroActivity.class);
+        startActivity( intent );
     }
 }
