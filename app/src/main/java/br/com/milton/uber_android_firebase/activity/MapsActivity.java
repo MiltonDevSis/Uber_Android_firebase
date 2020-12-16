59b3d3961edbc7d1bf4b7de7c9eda138ccd8f9b7
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-16 13:26:39 -0300

    Interface de autenticacao

--- app/src/main/java/br/com/milton/uber_android_firebase/activity/MapsActivity.java
+++ app/src/main/java/br/com/milton/uber_android_firebase/activity/MapsActivity.java
@@ -1,4 +1,4 @@
-package br.com.milton.uber_android_firebase;
+package br.com.milton.uber_android_firebase.activity;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import br.com.milton.uber_android_firebase.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
